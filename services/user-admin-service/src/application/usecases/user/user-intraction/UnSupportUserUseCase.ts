0f340732c5044cc6aa61ca537fe12b5b458dd119
Ajai-3 <ajaipjayan@gmail.com>
2025-08-18 15:15:54 +0530

    feat(user-interaction): add unsupport user use case implementation
    Implement use case for removing support relationship between users. Includes validation checks for user existence and support status before removing the relationship.

---
+++ services/user-admin-service/src/application/usecases/user/user-intraction/UnSupportUserUseCase.ts
@@ -0,0 +1,40 @@
+import { SupportUnSupportDto } from "../../../../domain/dtos/user/SupportUnSupportDto";
+import { IUserRepository } from "../../../../domain/repositories/user/IUserRepository";
+import { ISupporterRepository } from "../../../../domain/repositories/user/ISupporterRepository";
+import { BadRequestError, NotFoundError } from "art-chain-shared";
+import { USER_MESSAGES } from "../../../../constants/userMessages";
+import { AUTH_MESSAGES } from "../../../../constants/authMessages";
+
+export class UnSupportUserUseCase {
+  constructor(
+    private readonly _userRepo: IUserRepository,
+    private readonly _supporterRepo: ISupporterRepository
+  ) {}
+
+  async execute(data: SupportUnSupportDto): Promise<any> {
+    const { userId, currentUserId } = data;
+
+    if (!userId || !currentUserId) {
+      throw new BadRequestError(USER_MESSAGES.INVALID_SUPPORT_REQUEST);
+    }
+
+    const supporter = await this._userRepo.findById(currentUserId);
+    if (!supporter) {
+      throw new NotFoundError(AUTH_MESSAGES.USER_NOT_FOUND);
+    }
+    const targetUser = await this._userRepo.findById(userId);
+    if (!targetUser) {
+      throw new NotFoundError(AUTH_MESSAGES.USER_NOT_FOUND);
+    }
+
+    const isSupporting = await this._supporterRepo.isSupporting(
+      supporter.id,
+      targetUser.id
+    );
+    if (!isSupporting) {
+      throw new BadRequestError(USER_MESSAGES.NOT_SUPPORTING);
+    }
+
+    await this._supporterRepo.removeSupport(supporter.id, targetUser.id);
+  }
+}
