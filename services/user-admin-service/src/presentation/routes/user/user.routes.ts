d4a7b25d757e81600c18d5846e8a1e69c7f57cfc
Ajai-3 <ajaipjayan@gmail.com>
2025-08-19 12:29:12 +0530

    feat(logging): add winston logging with elasticsearch integration
    - for testing porpus
    - Add winston and winston-elasticsearch dependencies
    - Implement logging middleware for user routes
    - Add endpoints for testing info, warning and error logs

--- services/user-admin-service/src/presentation/routes/user/user.routes.ts
+++ services/user-admin-service/src/presentation/routes/user/user.routes.ts
@@ -5,6 +5,9 @@ import { SupporterRepositoryImpl } from "./../../../infrastructure/repositories/
 
 const router = express.Router();
 
+import { Request, Response, NextFunction } from "express";
+import { logger, storeUserInfo } from "../../../logger/logger";
+
 const userRepo = new UserRepositoryImpl();
 const suppoterRepo = new SupporterRepositoryImpl();
 const userController = new UserController(userRepo, suppoterRepo);
@@ -14,5 +17,41 @@ router.get("/profile/:userId", userController.getUserProfileWithId);
 
 router.post("/support/:userId", userController.supportUser);
 router.delete("/un-support/:userId", userController.unSupportUser);
+
+router.get("/info", async (req: Request, res: Response, next: NextFunction) => {
+ try {
+    const user = {
+      userId: "123",
+      name: "Ajai",
+      username: "ajai123",
+      email: "ajai@example.com"
+    };
+
+    logger.info("User created", { userId: user.userId });
+
+    await storeUserInfo(user);
+
+    res.send({ status: "success", message: "Info logs created ✅" });
+  } catch (err) {
+    logger.error("Error storing user info", { error: err });
+    res.status(500).send({
+      status: "error",
+      error: {
+        code: "InternalServerError",
+        message: (err as Error).message,
+        statusCode: 500
+      }
+    });
+  }
+});
+router.get("/warn", (req: Request, res: Response, next: NextFunction) => {
+  logger.warn("This is a warning");
+  res.send("Warning log created ⚠️");
+});
+
+router.get("/error", (req: Request, res: Response, next: NextFunction) => {
+  logger.error("Something went wrong", { errorCode: 500 });
+  res.send("Error log created ❌");
+});
 
 export default router;
