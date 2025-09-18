2cd50ca29092504800129edf69f416604554f209
Ajai-3 <ajaipjayan@gmail.com>
2025-09-18 15:21:36 +0530

    fix(validators): increase max length for category name to 20 characters

--- services/art-service/src/presentation/validators/editCategorySchema.ts
+++ services/art-service/src/presentation/validators/editCategorySchema.ts
@@ -1,7 +1,7 @@
 import z from "zod"
 
 export const editCategorySchema = z.object({
-    name: z.string().min(1, "Category name is required").max(15, "Maximum 15 characters").optional(),
+    name: z.string().min(1, "Category name is required").max(20, "Maximum 20 characters").optional(),
     count: z.number().optional(),
    status: z.enum(["active", "inactive"]).optional(),
 })
