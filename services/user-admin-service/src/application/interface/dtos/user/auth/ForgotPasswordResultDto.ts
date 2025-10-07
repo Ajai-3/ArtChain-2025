cbb948226506b9a35fb91291d9651dc0725de2ce
Ajai-3 <ajaipjayan@gmail.com>
2025-10-07 17:31:15 +0530

    style: standardize import quotes in AuthResultDto, ForgotPasswordResultDto, and GetCurrentUserResultDto

--- services/user-admin-service/src/application/interface/dtos/user/auth/ForgotPasswordResultDto.ts
+++ services/user-admin-service/src/application/interface/dtos/user/auth/ForgotPasswordResultDto.ts
@@ -1,4 +1,4 @@
-import { SafeUser } from "../../../../../domain/repositories/IBaseRepository";
+import { SafeUser } from '../../../../../domain/repositories/IBaseRepository';
 
 export interface ForgotPasswordResultDto {
   user: SafeUser;
