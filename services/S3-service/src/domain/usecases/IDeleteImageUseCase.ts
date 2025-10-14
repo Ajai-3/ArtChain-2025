ce5e6f2b0d50b88371059a128e91d64b1e154e32
Ajai-3 <ajaipjayan@gmail.com>
2025-10-14 12:22:38 +0530

    refactor: Remove unused image upload use cases and related DTOs to streamline the S3 service

--- services/S3-service/src/domain/usecases/IDeleteImageUseCase.ts
+++
@@ -1,5 +0,0 @@
-import { DeleteImageRequestDTO } from "../dto/DeleteImageRequestDTO";
-
-export interface IDeleteImageUseCase {
-    execute(request: DeleteImageRequestDTO): Promise<void>
-}
--- services/S3-service/src/domain/usecases/IUploadArtImage.ts
+++
@@ -1,6 +0,0 @@
-import { UploadFileDTO } from "../../domain/dto/UploadFileDTO";
-import { UploadedFileDTO } from "../../domain/dto/UploadedFileDTO";
-
-export interface IUploadArtImage {
-  execute(data: UploadFileDTO): Promise<UploadedFileDTO>;
-}
