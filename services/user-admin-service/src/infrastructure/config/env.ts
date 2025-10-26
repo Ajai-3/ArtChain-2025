1317a00357bfa80fc9652bbfa54080fd95e48120
Ajai-3 <ajaipjayan@gmail.com>
2025-10-26 19:40:54 +0530

    added new fields in the .env ts file for the testing

--- services/user-admin-service/src/infrastructure/config/env.ts
+++ services/user-admin-service/src/infrastructure/config/env.ts
@@ -1,35 +1,35 @@
-import 'dotenv-flow/config';
-import jwt from 'jsonwebtoken';
-import { getArtChainSecrets } from 'art-chain-shared';
+import "dotenv-flow/config";
+import jwt from "jsonwebtoken";
+import { getArtChainSecrets } from "art-chain-shared";
 
 const secrets = await getArtChainSecrets("ArtChainCommonSecret");
 
-
 export const config = {
-  port: process.env.PORT || '4001',
+  port: process.env.PORT || "4001",
   frontend_URL: process.env.FRONTEND_URL,
-  art_service_URL: process.env.ART_SERVICE_URL || '',
-  rabbitmq_URL: process.env.RABBITMQ_URL || '',
-  isProduction: process.env.NODE_ENV === 'production',
+  aws_cdn_domain: secrets.aws_cdn_domain,
+  rabbitmq_URL: process.env.RABBITMQ_URL || "",
+  art_service_URL: process.env.ART_SERVICE_URL || "",
+  isProduction: process.env.NODE_ENV === "production",
   jwt: {
     accessSecret: secrets.jwtAccessSecret,
     refreshSecret: secrets.jwtRefreshSecret,
     emailVerificationSecret: secrets.emailVerificationSecret,
     accessExpire: (process.env.JWT_ACCESS_EXPIRES_IN ||
-      '5m') as jwt.SignOptions['expiresIn'],
+      "5m") as jwt.SignOptions["expiresIn"],
     refreshExpire: (process.env.JWT_REFRESH_EXPIRES_IN ||
-      '30d') as jwt.SignOptions['expiresIn'],
+      "30d") as jwt.SignOptions["expiresIn"],
     emailVerificationExpire: (process.env.JWT_EMAIL_VERIFICATION_EXPIRES_IN ||
-      '5m') as jwt.SignOptions['expiresIn'],
+      "5m") as jwt.SignOptions["expiresIn"],
   },
   firebase: {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
   },
-   grpc: {
-    host: process.env.ELASTIC_GRPC_HOST || 'localhost',
-    port: process.env.ELASTIC_GRPC_PORT || '50051',
+  grpc: {
+    host: process.env.ELASTIC_GRPC_HOST || "localhost",
+    port: process.env.ELASTIC_GRPC_PORT || "50051",
   },
 };
 
