d12aaffdeb282a7fe1cde51ba1760da9a4affcdb
Ajai-3 <ajaipjayan@gmail.com>
2025-10-26 19:35:56 +0530

    change in the artwork fetching
    - insted of refatch always changed.
    - add staletime 5 minute to chache the responce

--- frontend/src/features/user/hooks/profile/gallery/useGetUserArt.ts
+++ frontend/src/features/user/hooks/profile/gallery/useGetUserArt.ts
@@ -2,25 +2,23 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import apiClient from "../../../../../api/axios";
 import type { PaginatedResponse } from "../../art/useGetAllArt";
 
-
 export const useGetUserArt = (userId: string) => {
   return useInfiniteQuery<PaginatedResponse, Error>({
     queryKey: ["userGallery", userId],
     queryFn: async ({ pageParam = 1 }) => {
       if (!userId) throw new Error("User ID is required");
 
-      const res = await apiClient.get(`/api/v1/art/user/${userId}`, 
-        { params: { page: pageParam, limit: 15 } } 
-      );
+      const res = await apiClient.get(`/api/v1/art/user/${userId}`, {
+        params: { page: pageParam, limit: 15 },
+      });
 
       return res.data as PaginatedResponse;
     },
-    getNextPageParam: (lastPage) =>
+    getNextPageParam: (lastPage: PaginatedResponse) =>
       lastPage.data.length < 15 ? undefined : lastPage.page + 1,
     initialPageParam: 1,
-    refetchOnMount: "always",
+    staleTime: 5 * 60 * 1000,
     refetchOnWindowFocus: false,
     enabled: !!userId,
   });
 };
-
