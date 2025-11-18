3998bc3de4c268822f1cc4904ecf6c98be78e7a4
Ajai-3 <ajaipjayan@gmail.com>
2025-11-18 13:03:51 +0530

    feat(user): add user data mapping and update user service
    Add mapToUserDto function to transform user data into UserDto format.
    Update UserService to use the new mapper and correct API endpoint path.

--- services/chat-service/src/infrastructure/http/UserService.ts
+++ services/chat-service/src/infrastructure/http/UserService.ts
@@ -3,6 +3,7 @@ import { env } from "../config/env"
 import { injectable } from "inversify";
 import { UserDto } from "../../applications/interface/dto/MessageResponseDto";
 import { IUserService } from "../../applications/interface/http/IUserService";
+import { mapToUserDto } from "../../applications/mappers/mapUser";
 
 @injectable()
 export class UserService implements IUserService {
@@ -10,8 +11,10 @@ export class UserService implements IUserService {
 
   async getUserById(userId: string): Promise<UserDto> {
     try {
-      const res = await axios.get<UserDto>(`${this.baseUrl}/api/v1/profile-id/${userId}`);
-      return res.data;
+      const res = await axios.get<{ data: UserDto }>(
+        `${this.baseUrl}/api/v1/user/profile-id/${userId}`
+      );
+      return mapToUserDto(res.data.data);
     } catch (err) {
       return {} as UserDto;
     }
