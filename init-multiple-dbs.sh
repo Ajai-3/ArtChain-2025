660e39547d6e74cf52f3e0bb716d34c6329250bf
Ajai <ajaipjayan@gmail.com>
2026-03-11 17:17:38 +0530

    chore: rename service databases to use underscores
    Update database names from hyphenated service names to underscore-separated names for consistency with naming conventions. The change affects user, wallet, and test service databases.

--- init-multiple-dbs.sh
+++ init-multiple-dbs.sh
@@ -3,11 +3,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 CREATE DATABASE artchain;
-CREATE DATABASE "artchain-user-admin-service";
-CREATE DATABASE "artchain-wallet-service";
-CREATE DATABASE "artchain-test-service";
+CREATE DATABASE "artchain_user_db";
+CREATE DATABASE "artchain_wallet_db";
+CREATE DATABASE "artchain_test_db";
 GRANT ALL PRIVILEGES ON DATABASE artchain TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE "artchain-user-admin-service" TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE "artchain-wallet-service" TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE "artchain-test-service" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "artchain_user_db" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "artchain_wallet_db" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "artchain_test_db" TO postgres;
 EOSQL
